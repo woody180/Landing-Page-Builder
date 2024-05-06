5eea726cd895a7a167753e393bd6ceb78b0ee553
woody180 <woody180@rocketmail.com>
2024-05-06 13:42:51 +0400

    Check if the element going to be shown.

--- app/Views/elements/slider.php
+++ app/Views/elements/slider.php
@@ -1,3 +1,4 @@
+<?php if (property_exists($section, 'slider') && $section->slider->show): ?>
 <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
 
     <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-5@m uk-grid">
@@ -14,3 +15,4 @@
     <a class="uk-position-center-right uk-position-small uk-hidden-hover" href uk-slidenav-next uk-slider-item="next"></a>
 
 </div>
+<?php endif; ?>
