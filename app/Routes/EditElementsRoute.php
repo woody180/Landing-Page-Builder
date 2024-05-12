fadf9ab8bdd720befaf48960beca4b7df6446322
woody180 <woody180@rocketmail.com>
2024-05-12 10:36:44 +0400

    update element edit router file

--- app/Routes/EditElementsRoute.php
+++ app/Routes/EditElementsRoute.php
@@ -11,16 +11,20 @@ $router = Router::getInstance();
 $router->get('load-editable-element/accordion', function($req, $res)
 {
     $model = initModel('section');
-    
+
+    $pageurl = query('pageurl') === '/' ? '' : query('pageurl');
+
     // Find page
-    $page = initModel('pages')->home();
+    $page = R::findOne('page', 'url = ?', [$pageurl]);
+
+    if (!$page) return $res->status(404)->send(['error' => 'Page not found']);
     
     // Get accordion items
-    $faq = $model->getSection($page->id, 'faq', false);
-    
+    $faq = $model->getPageRelatedElement($page->id, 'faq', false);
+
+    if (empty($faq)) return $res->status(404)->send(['error' => 'Element not found.']);
     
     // Send items back to front
-    // return $req->send(["section_id" => $faq->id, "data" => json_decode($faq->body)->items]);
     return $res->render('admin/elementEdit/accordion', [
         "data" => json_decode($faq->body)->items,
         "id" => $faq->id
@@ -37,18 +41,23 @@ $router->get('load-editable-element/slider', function($req, $res)
 {
     $model = initModel('section');
 
+    $pageurl = query('pageurl') === '/' ? '' : query('pageurl');
+
     // Find page
-    $page = initModel('pages')->home();
+    $page = R::findOne('page', 'url = ?', [$pageurl]);
+
+    if (!$page) return $res->status(404)->send(['error' => 'Page not found']);
     
     // Get accordion items
-    $slider = $model->getSection($page->id, 'partners', false);
-    
-    
+    $partners = $model->getPageRelatedElement($page->id, 'partners', false);
+
+    if (empty($partners)) return $res->status(404)->send(['error' => 'Element not found.']);
+
+
     // Send items back to front
-    // return $req->send(["section_id" => $faq->id, "data" => json_decode($faq->body)->items]);
-    return $res->render('admin/elementEdit/slider', [
-        "data" => json_decode($slider->body)->items,
-        "id" => $slider->id
+    return $res->render('admin/elementEdit/logo-slider', [
+        "data" => json_decode($partners->body)->items,
+        "id" => $partners->id
     ]);
     
 }, ['Middlewares/checkAdmin', 'Middlewares/checkAjax']);
