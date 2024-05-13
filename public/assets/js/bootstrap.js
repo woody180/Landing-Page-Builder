552d105f4517dead73bf60e1ee32dc2729937f3c
woody180 <woody180@rocketmail.com>
2024-05-13 11:11:01 +0400

    update bootstrap.js

--- public/assets/js/bootstrap.js
+++ public/assets/js/bootstrap.js
@@ -9,8 +9,11 @@ import UtilitiesController from '../js/classes/UtilitiesController.js';
 import UiController from '../js/classes/UiController.js';
 import SectionsController from '../js/classes/SectionsController.js';
 
+
+// Import editable element here
 import Accordion from '../js/classes/elementsEdit/Accordion.js';
 import Slider from '../js/classes/elementsEdit/Slider.js';
+import ImageSlider from '../js/classes/elementsEdit/ImageSlider.js';
 
 
 new PagesController(baseurl);
@@ -21,12 +24,12 @@ if (loggedin) new SectionsController(baseurl);
 
 
 
-
+// Initialization of editable elements from here
 if (loggedin) {
-    
     
     new Accordion(baseurl);
     new Slider(baseurl);
+    new ImageSlider(baseurl);
     
 
     new FgTinyEditor({
